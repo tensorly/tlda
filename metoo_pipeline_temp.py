4a750ed1412b183e5b45603f139c5aa1bc24a1e3
danielEbanks <56091180+danielEbanks@users.noreply.github.com>
2022-08-29 14:14:03 -0700

    Update metoo_pipeline_temp.py

--- metoo_pipeline_temp.py
+++ metoo_pipeline_temp.py
@@ -108,7 +108,7 @@ inDir  = "/raid/debanks/MeToo/data/MeTooMonthCleaned" # MeTooMonthCleaned" # inp
 # Learning parameters
 num_tops = 100 #100 # 50 topics :(931, 93, 1258) coherence: 2277 (lr=0.00003 )
 alpha_0 = 0.01
-batch_size_pca  = 300000  # this will handle 2000 words + 100 topics ad infinite number of documents 
+batch_size_pca  = 200000  # this will handle 2000 words + 100 topics ad infinite number of documents 
 #batch_size_pca  = 5000 # for whitening
 batch_size_grad = 250000 # 1% of data size - see what coherence looks like - can also try increasing  #divide data by 1,000 ## 800 = -3322.32 (6000 seecond) 4000=-3320 (1800 seconds) 8000=-3325 (1180 seconds)  Lower this to 1% of TOTAL data size
 n_iter_train    = 1000
