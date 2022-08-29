af992f0d3bbf9817e90323d0d16b5e84dc4dbab8
danielEbanks <56091180+danielEbanks@users.noreply.github.com>
2022-08-29 16:47:39 -0700

    Update third_order_cumulant.py

--- version0_99/third_order_cumulant.py
+++ version0_99/third_order_cumulant.py
@@ -153,6 +153,7 @@ class ThirdOrderCumulant():
         # factors = nvocab x ntopics
         n_topics = self.n_topic
         n_docs = X_batch.shape[0]
+        print(X_batch.shape[1])
 
         gammad = tl.tensor(tl.gamma(self.gamma_shape, scale= 1.0/self.gamma_shape, size = (n_docs,n_topics))) ## not working
         exp_elogthetad = tl.tensor(tl.exp(dirichlet_expectation(gammad))) #ndocs, n_topics
