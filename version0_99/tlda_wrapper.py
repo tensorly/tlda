b44381906a1a0f8b7934a6f4dbbd88747d0cf3eb
danielEbanks <56091180+danielEbanks@users.noreply.github.com>
2022-08-29 17:16:21 -0700

    Update tlda_wrapper.py

--- version0_99/tlda_wrapper.py
+++ version0_99/tlda_wrapper.py
@@ -178,7 +178,8 @@ class TLDA():
             set of documetns to predict topic distribution
         predict : indicate whether to return topic-document distribution and word-topic distribution or just word-topic distribution. 
         """
-        self.third_order.unwhitened_factors_ = self.unwhitened_factors_
+        self.third_order.unwhitened_factors_ = self.unwhitened_factors()
+        print(self.third_order.unwhitened_factors_)
         if predict:
             predicted_topics = self.third_order.predict(X, self.weights_)
             return predicted_topics
