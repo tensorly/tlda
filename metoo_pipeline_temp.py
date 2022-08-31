f71dd39afd2f4fce1d77286dbe407793b17dfeb9
danielEbanks <56091180+danielEbanks@users.noreply.github.com>
2022-08-30 19:11:20 -0700

    Update metoo_pipeline_temp.py

--- metoo_pipeline_temp.py
+++ metoo_pipeline_temp.py
@@ -114,21 +114,21 @@ num_tops = 100 #100 # 50 topics :(931, 93, 1258) coherence: 2277 (lr=0.00003 )
 alpha_0 = 0.01
 batch_size_pca  = 50000  # this will handle 2000 words + 100 topics ad infinite number of documents 
 #batch_size_pca  = 5000 # for whitening
-batch_size_grad = 25000 # 1% of data size - see what coherence looks like - can also try increasing  #divide data by 1,000 ## 800 = -3322.32 (6000 seecond) 4000=-3320 (1800 seconds) 8000=-3325 (1180 seconds)  Lower this to 1% of TOTAL data size
+batch_size_grad = 250 # 1% of data size - see what coherence looks like - can also try increasing  #divide data by 1,000 ## 800 = -3322.32 (6000 seecond) 4000=-3320 (1800 seconds) 8000=-3325 (1180 seconds)  Lower this to 1% of TOTAL data size
 n_iter_train    = 1000
 n_iter_test     = 100
 # 0.0005
 learning_rate   = 0.005 # increase bc increased batch size #30 topics # 0.00001 8000=-3325 (1180 seconds); 0.00002 8000=-3321 (452 seconds); 0.00003 8000=-3322 (275 seconds);  0.00004 8000=-3322 (907 seconds);
 theta_param = 5.005
 smoothing   = 1e-5
-ortho_loss_param = 40
+ortho_loss_param = 100
 
 # Program controls
 split_files    = 0
 vocab_build    = 0
 save_files     = 0
-stgd           = 0
-transform_data    = 0
+stgd           = 1
+transform_data    = 1
 create_meta_df    = 1
 recover_top_words = 1
 coherence         = 1
